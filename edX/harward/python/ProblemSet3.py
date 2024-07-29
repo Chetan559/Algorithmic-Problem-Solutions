833a8c641bcbc3e24ff95392fe669fdc6867da6f
Chetan Sharma <cschetan559@gmail.com>
2024-07-30 00:23:55 +0530

    Problem Set 3.1 for week 3

---
+++ edX/harward/python/ProblemSet3.py
@@ -0,0 +1,76 @@
+'''
+Fuel gauges indicate, often with fractions, just how much fuel is in a tank. For instance 1/4 indicates that a tank is 25% full, 1/2 indicates that a tank is 50% full, and 3/4 indicates that a tank is 75% full.
+
+In a file called fuel.py, implement a program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is an integer, and then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank. If, though, 1% or less remains, output E instead to indicate that the tank is essentially empty. And if 99% or more remains, output F instead to indicate that the tank is essentially full.
+
+If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.
+'''
+
+import math
+while True:
+    fraction = input("Fraction:")
+    try:
+        x, y = fraction.split('/')
+        x = int(x)
+        y = int(y)
+        fuel = int(round((x/y)*100))
+    except (ValueError,ZeroDivisionError):
+        pass
+    else:
+        if x > y:
+            continue
+        elif 99 <= fuel:
+            print("F")
+            break
+        elif 0 <= fuel <=1 :
+            print("E")
+            break
+        else:
+            print(str(fuel)+'%')
+            break
+
+'''
+One of the most popular places to eat in Harvard Square is Felipe’s Taqueria, which offers a menu of entrees, per the dict below, wherein the value of each key is a price in dollars:
+
+{
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+In a file called taqueria.py, implement a program that enables a user to place an order, prompting them for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program). After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places. Treat the user’s input case insensitively. Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
+'''
+
+dict = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total = 0
+while True:
+    try:
+        item = input("Item: ").title()
+        total += dict[item]
+        print(f"Total: ${total:.2f}")
+    except EOFError:
+        print()
+        break
+    except KeyError:
+        pass
+
+
+
+
