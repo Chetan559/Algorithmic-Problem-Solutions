558badfd7b17566ef106950e0afbeb2156cdc7fc
Chetan Sharma <cschetan559@gmail.com>
2025-04-02 01:55:28 +0530

    Time: 4 ms (40.26%), Space: 18.40MB (79.29%) - Leetcode

--- Leetcode/73. Set Matrix Zeroes.py
+++ Leetcode/73. Set Matrix Zeroes.py
@@ -20,3 +20,29 @@ class Solution:
             for col in range(cols):
                 if matrix[row][col] == 'a':
                     matrix[row][col] = 0
+
+
+# better solution
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
+        colz = [1] * cols
+        rowz = [1] * rows
+        for row in range(rows):
+            for col in range(cols):
+                if matrix[row][col] == 0:
+                    rowz[row] =0
+                    colz[col] = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                if (rowz[row] == 0 or colz[col] == 0):
+                    matrix[row][col] = 0
+
+                
+
