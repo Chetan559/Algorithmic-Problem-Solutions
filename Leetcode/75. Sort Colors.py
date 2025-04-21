ee4ed1848c175d4f8225df560d301241efdf9bc9
Chetan Sharma <cschetan559@gmail.com>
2025-04-21 16:26:36 +0530

    Time: 0 ms (100.00%), Space: 17.72 MB (53.91%) - Leetcode

--- Leetcode/75. Sort Colors.py
+++ Leetcode/75. Sort Colors.py
@@ -3,6 +3,34 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        low = mid = 0
+        high = len(nums) - 1
+
+        while high >= mid:
+            if nums[mid] == 0:
+                nums[mid], nums[low] = nums[low], nums[mid]
+                mid += 1
+                low += 1
+            
+            elif nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+            
+            elif nums[mid] == 1:
+                mid += 1
+                
+# in above code problem is solved uisng dutch national flag algorithm
+# in this algorithm we are using three pointers low, mid and high
+# below was my intiutional solution which is not optimal but it works
+# and is easy to understand
+
+'''
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         nums0 = 0
         nums1 = 0
         nums2 = 0
@@ -27,3 +55,4 @@ class Solution:
             nums[i] = 1
         for i in range(nums0 + nums1, nums0 + nums1 + nums2):
             nums[i] = 2
+'''
