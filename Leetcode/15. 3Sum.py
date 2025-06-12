8c14f6b2d5462c13262c3517bf51159dc268a1e5
Chetan Sharma <cschetan559@gmail.com>
2025-06-12 14:13:58 +0530

    Time: 844 ms (23.00%), Space: 20.61 MB (68.97%) - Leetcode

--- Leetcode/15. 3Sum.py
+++ Leetcode/15. 3Sum.py
@@ -14,17 +14,11 @@ class Solution:
                     j += 1
                     k -= 1
 
-                    while j < k:
-                        if nums[j] == nums[j-1]:
-                            j += 1 
-                        else:
-                            break
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1 
                     
-                    while j < k:
-                        if nums[k] == nums[k+1]:
-                            k -= 1
-                        else:
-                            break
+                    while  nums[k] == nums[k+1] and j < k:
+                        k -= 1
 
                 elif nums[j] + nums[k] > -nums[i]:
                     k -= 1
