196528f8faff2ab948e1d9c1c7576684d29444d9
Chetan Sharma <cschetan559@gmail.com>
2025-07-06 22:29:44 +0530

    Time: 2 ms (53.28%), Space: 19.09 MB (23.85%) - Leetcode

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -1,7 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #  brute force
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             return [i, j]
 
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
+        #  hashmap
+        hashMap = {}
+
+        for i in range(len(nums)):
+            if (target - nums[i]) in hashMap:
+                return [i, hashMap[target - nums[i]]]
+            else:
+                hashMap[nums[i]] = i
