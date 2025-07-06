ed55572ecc2bffc8c0a3cc846660f1f1439fb43a
Chetan Sharma <cschetan559@gmail.com>
2025-07-06 22:45:12 +0530

    Time: 3 ms (41.79%), Space: 17.70 MB (86.40%) - Leetcode

---
+++ Leetcode/202. Happy Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+
+        def get_next(num):
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit*digit
+                num = num // 10
+
+            return  total
+
+        seen = set()
+
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+
+        return (n == 1)
